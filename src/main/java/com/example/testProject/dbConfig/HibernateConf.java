8143c2711a4fa39441339e205d0522d36704c6a6
Konstantinos Paidarakis <kpaidarakis@gmail.com>
2021-02-07 00:19:11 +0200

    set configuration for hibernate, set hibernate properties, auto create employees table in the database.

---
+++ src/main/java/com/example/testProject/dbConfig/HibernateConf.java
@@ -0,0 +1,57 @@
+package com.example.testProject.dbConfig;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConf {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactoryBean() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.example.testProject.model");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(env.getProperty("datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("datasource.url"));
+        dataSource.setUsername(env.getProperty("datasource.username"));
+        dataSource.setPassword(env.getProperty("datasource.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+        hibernateTransactionManager.setSessionFactory(sessionFactoryBean().getObject());
+        return hibernateTransactionManager;
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        hibernateProperties.setProperty("hibernate.format_sql", "true");
+        return hibernateProperties;
+    }
+}
--- src/main/java/com/example/testProject/model/Employee.java
+++ src/main/java/com/example/testProject/model/Employee.java
@@ -1,27 +1,118 @@
 package com.example.testProject.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
-@Entity
+@Entity(name = "EMPLOYEES")
 public class Employee implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
     private Long id;
     @Column
     private String name;
     @Column
-    private String lastName;
-    @Column
+    private String last_name;
+    @Column(nullable = false, unique = true)
     private String email;
     @Column
-    private String jobTitle;
-    @Column
+    private String job_title;
+    @Column(unique = true)
     private String phone;
     @Column
-    private String imageUrl;
+    private String image_url;
     @Column
-    private String employeeCode;
+    private String employee_code;
 
+    public Employee() {
+    }
+
+    public Employee(Long id, String name, String last_name, String email, String job_title, String phone, String image_url, String employee_code) {
+        this.id = id;
+        this.name = name;
+        this.last_name = last_name;
+        this.email = email;
+        this.job_title = job_title;
+        this.phone = phone;
+        this.image_url = image_url;
+        this.employee_code = employee_code;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJob_title() {
+        return job_title;
+    }
+
+    public void setJob_title(String job_title) {
+        this.job_title = job_title;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public String getEmployee_code() {
+        return employee_code;
+    }
+
+    public void setEmployee_code(String employee_code) {
+        this.employee_code = employee_code;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", email='" + email + '\'' +
+                ", job_title='" + job_title + '\'' +
+                ", phone='" + phone + '\'' +
+                ", image_url='" + image_url + '\'' +
+                ", employee_code='" + employee_code + '\'' +
+                '}';
+    }
 }
