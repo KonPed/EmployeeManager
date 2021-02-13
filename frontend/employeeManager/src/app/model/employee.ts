8f403333cda5ea664e7d08d9b44f7303b735b298
Konstantinos Paidarakis <kpaidarakis@gmail.com>
2021-02-13 21:23:17 +0200

    change property name on Employee model.

--- frontend/employeeManager/src/app/model/employee.ts
+++ frontend/employeeManager/src/app/model/employee.ts
@@ -1,10 +1,10 @@
 export interface Employee {
   id: number;
   name: string;
-  last_name: string;
+  lastName: string;
   email: string;
-  job_title: string;
+  jobTitle: string;
   phone: string
-  image_url: string
-  employee_code: string;
+  imageUrl: string
+  employeeCode: string;
 }
